983d8ce0199f1a5498b71c3cac8a0e010107b839
Ross Andreucetti <ross.andreucetti@newswhip.com>
2017-01-20 12:25:53 +0000

    forgot to commit file

--- ec2Bootstrap/userData.sh
+++ ec2Bootstrap/userData.sh
@@ -9,6 +9,8 @@ apt_get_install()
         install $@
 }
 
+ES_ACTORS_VERSION=$ES_ACTORS_VERSION
+
 # Mark execution start
 echo "STARTING" > /root/user_data_run
 
@@ -35,12 +37,14 @@ apt_get_install sbt
 
 mkdir /opt/elasticsearch-migration
 cd /opt/elasticsearch-migration
-wget https://github.com/NewsWhip/es-actors/archive/v1.3.9.tar.gz
-tar -xvf v1.3.9.tar.gz
-cd es-actors-1.3.9
+wget https://github.com/NewsWhip/es-actors/archive/v$ES_ACTORS_VERSION.tar.gz
+tar -xvf v$ES_ACTORS_VERSION.tar.gz
+cd es-actors-$ES_ACTORS_VERSION/es-actors
 SERVER_CMD="sbt -J-Xmx30G -J-Xms30G \"; project server; run-main com.broilogabriel.Server\""
 eval $SERVER_CMD </dev/null &>/dev/null &
-/opt/elasticsearch-migration/es-actors-1.3.9/ec2Bootstrap/nightly.sh /opt/elasticsearch-migration/es-actors-1.3.9/ NewsWhipCluster NewsWhipStagingCluster 10.0.1.10,10.0.3.10,10.0.7.10,10.0.9.10 10.0.1.110,10.0.3.110,10.0.7.110,10.0.9.110 9300 8 >/dev/null 2>&1 &
+sleep 100
+chmod 755 /opt/elasticsearch-migration/es-actors-$ES_ACTORS_VERSION/ec2Bootstrap/nightly.sh
+/opt/elasticsearch-migration/es-actors-$ES_ACTORS_VERSION/ec2Bootstrap/nightly.sh /opt/elasticsearch-migration/es-actors-$ES_ACTORS_VERSION/es-actors NewsWhipCluster NewsWhipStagingCluster 10.0.1.10,10.0.3.10,10.0.7.10,10.0.9.10 10.0.1.110,10.0.3.110,10.0.7.110,10.0.9.110 9300 8 >/dev/null 2>&1 &
 
 # Mark execution end
 echo "DONE" > /root/user_data_run
