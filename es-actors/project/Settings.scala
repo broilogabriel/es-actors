c6cf05d6dfa4104bbdc5832187a0b3a9f2744106
Ross Andreucetti <ross.andreucetti@newswhip.com>
2017-01-20 12:26:42 +0000

    typo

--- es-actors/project/Settings.scala
+++ es-actors/project/Settings.scala
@@ -28,7 +28,7 @@ object Settings extends Dependencies {
 
   private val commonSettings = Seq(
     organization := defaultOrg,
-    version := "1.3.11",
+    version := "1.3.12",
     scalaVersion := scalaVersionUsed
   )
 
