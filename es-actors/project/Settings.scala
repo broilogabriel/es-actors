37c05ae089cc75c876968bfc5582b57eeb13abcc
Ross Andreucetti <ross.andreucetti@newswhip.com>
2017-01-20 11:55:05 +0000

    dynamic version

--- es-actors/project/Settings.scala
+++ es-actors/project/Settings.scala
@@ -28,7 +28,7 @@ object Settings extends Dependencies {
 
   private val commonSettings = Seq(
     organization := defaultOrg,
-    version := "1.3.10",
+    version := "1.3.11",
     scalaVersion := scalaVersionUsed
   )
 
