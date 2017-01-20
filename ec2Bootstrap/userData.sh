e91b3c9bb0ea9206e37ae480f8256b8c71a8a19c
Ross Andreucetti <ross.andreucetti@newswhip.com>
2017-01-20 11:30:10 +0000

    remove sudo, update versions

--- ec2Bootstrap/userData.sh
+++ ec2Bootstrap/userData.sh
@@ -22,7 +22,7 @@ apt_get_install git
 
 # Instal java8 
 echo debconf shared/accepted-oracle-license-v1-1 select true | debconf-set-selections
-debconf shared/accepted-oracle-license-v1-1 seen true | sudo debconf-set-selections
+echo debconf shared/accepted-oracle-license-v1-1 seen true | debconf-set-selections
 add-apt-repository ppa:webupd8team/java
 apt-get update
 apt_get_install oracle-java8-installer
@@ -35,12 +35,12 @@ apt_get_install sbt
 
 mkdir /opt/elasticsearch-migration
 cd /opt/elasticsearch-migration
-wget https://github.com/NewsWhip/es-actors/archive/v1.3.7.tar.gz
-tar -xvf v1.3.7.tar.gz
-cd es-actors-1.3.7
-SERVER_CMD="sbt -J-Xmx32G -J-Xms32G \"; project server; run-main com.broilogabriel.Server\""
+wget https://github.com/NewsWhip/es-actors/archive/v1.3.9.tar.gz
+tar -xvf v1.3.9.tar.gz
+cd es-actors-1.3.9
+SERVER_CMD="sbt -J-Xmx30G -J-Xms30G \"; project server; run-main com.broilogabriel.Server\""
 eval $SERVER_CMD </dev/null &>/dev/null &
-/opt/elasticsearch-migration/es-actors-1.3.7/ec2Bootstrap/nightly.sh /opt/elasticsearch-migration/es-actors-1.3.7/ NewsWhipCluster NewsWhipStagingCluster 10.0.1.10,10.0.3.10,10.0.7.10,10.0.9.10 10.0.1.110,10.0.3.110,10.0.7.110,10.0.9.110 9300 8 >/dev/null 2>&1 &
+/opt/elasticsearch-migration/es-actors-1.3.9/ec2Bootstrap/nightly.sh /opt/elasticsearch-migration/es-actors-1.3.9/ NewsWhipCluster NewsWhipStagingCluster 10.0.1.10,10.0.3.10,10.0.7.10,10.0.9.10 10.0.1.110,10.0.3.110,10.0.7.110,10.0.9.110 9300 8 >/dev/null 2>&1 &
 
 # Mark execution end
 echo "DONE" > /root/user_data_run
