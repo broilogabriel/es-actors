6cf455d1347f5bcf8907c1768edd6bf6f2ac5597
Ross Andreucetti <ross.andreucetti@newswhip.com>
2017-05-11 16:07:57 +0100

    add RC in version

--- es-actors/project/Settings.scala
+++ es-actors/project/Settings.scala
@@ -28,7 +28,7 @@ object Settings extends Dependencies {
 
   private val commonSettings = Seq(
     organization := defaultOrg,
-    version := "2.0.0",
+    version := "2.0.0-RC",
     scalaVersion := scalaVersionUsed
   )
 
