dbc35b92857536b6f7a37fb9a5503bbb2ca6e49d
Ross Andreucetti <ross.andreucetti@newswhip.com>
2017-05-11 14:34:46 +0100

    update version

--- es-actors/project/Settings.scala
+++ es-actors/project/Settings.scala
@@ -28,7 +28,7 @@ object Settings extends Dependencies {
 
   private val commonSettings = Seq(
     organization := defaultOrg,
-    version := "1.3.33",
+    version := "2.0.0",
     scalaVersion := scalaVersionUsed
   )
 
